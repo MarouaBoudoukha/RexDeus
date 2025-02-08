a07712119a5758eaa11d88901bc7b1a8992b30f6
SamBouu <SBoudechicha@stealth-power.com>
2025-02-08 14:45:01 +0100

    add unreal engine repo

---
+++ Unreal Engine MotherAI/Source/MotherAI/MotherAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MotherAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMotherAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMotherAIGameMode();
+};
+
+
+
